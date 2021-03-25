37750bab33b9b7954cb09074ccd85efe5a9305fc
malefooo <31642966+malefooo@users.noreply.github.com>
2021-03-25 15:08:52 +0800

    Create 获取随机因子.js

---
+++ 4.辅助功能/获取随机因子.js
@@ -0,0 +1,16 @@
+export const data = {
+  "cURL": [
+    {
+      title: "请求示例",
+      value: ""
+    },
+    {
+      title: "返回示例",
+      value: `{
+        "code":0,
+        "message":"success",
+        "data":"6bb8f74ed7254d018ebcf1ad35f9f95e"
+      }`
+    }
+  ]
+}
